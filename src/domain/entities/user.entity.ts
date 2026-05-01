6b3321321b1515dea8992c8ee032b158a9b87c26
Carlos Albillos <carlos@email.com>
2026-05-01 17:32:23 +0200

    New domain: user

--- src/domain/entities/user.entity.ts
+++ src/domain/entities/user.entity.ts
@@ -6,3 +6,37 @@ export interface User {
   role: string;
   is_active: boolean;
 }
+
+export interface CreateUserDTO {
+  id: string;
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+  role: string;
+}
+
+export interface UpdateUserDTO {
+  name?: string;
+  surname?: string;
+  email?: string;
+  role?: string;
+  is_active?: boolean;
+}
+
+export interface TimeEntry {
+  id: string;
+  project_user_id: string;
+  date: string;
+  hour: number;
+  comment: string;
+  project: {
+    id: string;
+    name: string;
+  };
+}
+
+export interface TimeEntriesResponse {
+  total_hours: number;
+  data: TimeEntry[];
+}
--- src/domain/ports/UserRepository.ts
+++ src/domain/ports/UserRepository.ts
@@ -1,7 +1,11 @@
-import type { User } from "../entities/user.entity";
+import type { User, CreateUserDTO, UpdateUserDTO, TimeEntriesResponse } from "../entities/user.entity";
 import type { Project } from "../entities/project.entity";
 
 export interface UserRepository {
+  getUsers(): Promise<User[]>;
   getById(id: string): Promise<User>;
+  createUser(dto: CreateUserDTO): Promise<void>;
+  updateUser(id: string, dto: UpdateUserDTO): Promise<void>;
   getUserProjects(id: string): Promise<Project[]>;
+  getUserTimeEntries(id: string): Promise<TimeEntriesResponse>;
 }
