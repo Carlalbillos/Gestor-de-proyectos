f370acc81235cac62058c443ed96c401040fb1ba
Carlos Albillos <carlos@email.com>
2026-05-15 10:02:32 +0200

    feat: add is_main property to contact creation payload in ApiClientRepository

--- src/infrastructure/adapters/ApiClientRepository.ts
+++ src/infrastructure/adapters/ApiClientRepository.ts
@@ -72,6 +72,7 @@ export class ApiClientRepository implements ClientRepository {
       full_name: contact.fullName,
       phone_number: contact.phoneNumber || null,
       email: contact.email,
+      is_main: false,
       note: contact.note || null,
     });
   }
